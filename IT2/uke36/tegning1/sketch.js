13122781a604b828c2db187c4af86e679a47ee74
Alexanderauerdahl <42533075+Alexanderauerdahl@users.noreply.github.com>
2019-09-05 14:12:46 +0200

    36

---
+++ IT2/uke36/tegning1/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  // put setup code here
+  createCanvas(400, 400);
+  stroke(255);strokeWeight(4);
+  nofill();
+  if (mouseX>200){
+    fill(255,0,200)
+  }
+  rect(100,150,60,100)
+}
+
+function draw() {
+  // put drawing code here
+  background(220);
+
+}
